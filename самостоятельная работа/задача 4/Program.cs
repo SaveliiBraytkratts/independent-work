e8a84570504feecafa1adb3914f39278cf36d664
unknown <braytkratts@bk.ru>
2023-02-12 13:13:45 +1000

    задача 4 с первыми переменными

---
+++ самостоятельная работа/задача 4/Program.cs
@@ -0,0 +1,26 @@
+﻿double a, b, c;        
+a = 2;
+b = 3;
+c = 7;
+if (a >= b)
+{
+    if (a >= c)
+    {
+        Console.WriteLine(a);
+    }
+    else
+    {
+        Console.WriteLine(c);
+    }
+}
+else
+{
+    if (b >= c)
+    {
+        Console.WriteLine(b);
+    }
+    else
+    {
+    Console.WriteLine(c);
+    }
+}
