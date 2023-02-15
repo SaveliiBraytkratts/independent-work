b7381025a8a05a2ed2a6b538914ed2799f66078f
unknown <braytkratts@bk.ru>
2023-02-15 18:45:39 +1000

    Самостоятельная работа 2 задача 15

--- самостоятельная работа 2/задача 15/Program.cs
+++ самостоятельная работа 2/задача 15/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿int понедельник = 1;
+int вторник = 2;
+int среда = 3;
+int четверг = 4;
+int пятница = 5;
+int суббота = 6;
+int воскресение = 7;
+int number = int.Parse(Console.ReadLine());
+if (number <= 5 )
+{
+    Console.WriteLine("нет");
+}
+else
+{
+    Console.WriteLine("да");
+}
