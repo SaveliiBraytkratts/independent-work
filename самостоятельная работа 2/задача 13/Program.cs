c23f1ab7e5a4b268227777010816b926f9f8d9ec
unknown <braytkratts@bk.ru>
2023-02-15 18:21:21 +1000

    Самостоятельная работа 2 задача 13

---
+++ самостоятельная работа 2/задача 13/Program.cs
@@ -0,0 +1,10 @@
+﻿int number = int.Parse(Console.ReadLine());
+int result = (number / 100) % 10;
+if(result == 0 )
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    Console.WriteLine(result);
+}
