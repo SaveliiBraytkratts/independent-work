82f2a240044f42f26640a6d596550e6f512be494
unknown <braytkratts@bk.ru>
2023-02-19 19:39:14 +1000

    решение 3 задачи

---
+++ самостоятельная работа 3/задача 2/Program.cs
@@ -0,0 +1,14 @@
+﻿int index = 1;
+void TableCubesNumbers (int N)
+{
+    while (index <= N)
+    {
+        int Cubes = index * index * index;
+        index++;
+        Console.Write(Cubes);
+        Console.Write(", ");
+    } 
+}
+Console.Write("Введите число:");
+int N = int.Parse(Console.ReadLine()!);
+TableCubesNumbers (N);
