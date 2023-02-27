38193929445cfa73d5e1ad3e8c1285e4e9ea6c21
unknown <braytkratts@bk.ru>
2023-02-28 09:15:31 +1000

    решение первой задачи на возведение в степень

---
+++ самостоятельная работа 4/задача 1/Program.cs
@@ -0,0 +1,16 @@
+﻿int index = 1;
+Console.Write("Введите число:");
+int number = int.Parse(Console.ReadLine()!);
+int result = number;
+void naturalDegree (int number, int degree)
+{
+    while ( index < degree )
+    {
+        result = (result * number);
+        index++;
+    }
+}
+Console.Write("Введите степень:");
+int degree = int.Parse(Console.ReadLine()!);
+naturalDegree(number, degree );
+Console.Write(result);
