04c524f902d9128e3d81b9538d5d6e59eb5abe72
unknown <braytkratts@bk.ru>
2023-03-03 10:37:56 +1000

    решение задачи задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.

---
+++ самостоятелная работа 5/задача 1/Program.cs
@@ -0,0 +1,43 @@
+﻿void FillArray(int[] masiv)
+{
+    int length = masiv.Length;
+    int index = 0;
+    while (index < length)
+    {
+        masiv[index] = new Random().Next(100, 999);
+        index++;
+    }
+    
+}
+
+void PrintArray(int[] mas)
+{
+    int length = mas.Length;
+    int index = 0;
+    while (index < length)
+    {
+        Console.WriteLine(mas[index]);
+        index++;
+    }
+}
+
+void CheckEvenness(int[] number)
+{
+    int length = number.Length;
+    int index = 0;
+    int variable = 0;
+    while (index < length)
+    {
+        if (number[index] % 2 == 1)
+        {
+           variable = (variable + 1 );
+        }
+        index++;
+    }
+    Console.WriteLine(variable);
+}
+
+int[] arrey = new int[4];
+FillArray(arrey);
+PrintArray(arrey);
+CheckEvenness(arrey);
