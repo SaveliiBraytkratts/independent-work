b8fee371665c7db65fb8c35ceb867e5adbe8a47a
unknown <braytkratts@bk.ru>
2023-03-03 20:12:01 +1000

    решение задачи пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь

---
+++ IndependentWork6/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿int count = 0;
+void GreaterZero(int[] Arra)
+{
+    for (int i = 0; i < Arra.Length; i++)
+    {
+        if (Arra[i] > 0)
+        {
+            count++;
+        }
+    }
+}
+Console.Write("Введите элементы(через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+GreaterZero(arr);
+Console.WriteLine($"Кол-во элементов > 0: {count}");
