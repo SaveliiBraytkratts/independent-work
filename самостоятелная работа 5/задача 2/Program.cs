83e68373df2bf8211d7bffc62746fb89535ee337
unknown <braytkratts@bk.ru>
2023-03-03 10:49:50 +1000

    решение задачи задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях

---
+++ самостоятелная работа 5/задача 2/Program.cs
@@ -0,0 +1,41 @@
+﻿void FillArray(int[] masiv)
+{
+    int length = masiv.Length;
+    int index = 0;
+    while (index < length)
+    {
+        masiv[index] = new Random().Next(100, 999);
+        index++;
+    }
+    
+}
+
+void PrintArray(int[] mas)
+{
+    int length = mas.Length;
+    int index = 0;
+    while (index < length)
+    {
+        Console.WriteLine(mas[index]);
+        index++;
+    }
+}
+
+void CheckEvenness(int[] number)
+{
+    int length = number.Length;
+    int index = 1;
+    int variable = 0;
+    while (index < length)
+    {  
+    variable = (variable + number[index] );    
+    index = index + 2;
+    }
+    Console.WriteLine(" ");
+    Console.WriteLine(variable);
+}
+
+int[] arrey = new int[4];
+FillArray(arrey);
+PrintArray(arrey);
+CheckEvenness(arrey);
