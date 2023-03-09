8b79e926b43a6cd532b3cf60dab0faa8241496a3
unknown <braytkratts@bk.ru>
2023-03-09 22:19:27 +1000

    решение задачи Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.

--- IndependentWork8/Task3/Program.cs
+++ IndependentWork8/Task3/Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿void PrintIndex(int[,,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.WriteLine();
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                Console.Write($"{arr[i, j, k]}({i},{j},{k}) ");
+            }
+        }
+    }
+}
+
+void FillArray(int[,,] arr)
+{
+    int count = 10;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                arr[k, i, j] = arr[k, i, j] + count;
+                count = count + 3;
+            }
+        }
+    }
+}
+
+int[,,] array = new int[2, 2, 2];
+FillArray(array);
+PrintIndex(array);
